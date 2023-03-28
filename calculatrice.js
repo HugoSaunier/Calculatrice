6c826495602fd5611693e0567a9c6958b198a26a
HugoSaunier <90180914+HugoSaunier@users.noreply.github.com>
2023-03-28 23:14:38 +0200

    Fin étape 2(partie 3 et 4)

--- calculatrice.js
+++ calculatrice.js
@@ -1,19 +1,44 @@
-function Affiche(chiffre){                                 /*la fonction affiche ajoute le chiffre rentré par l'uttilisateur*/
-    var ecran = document.getElementById("ecran");          /*on créé un variable ecran afin de pouvoir modifier par la suite la valeur de la case*/
-    ecran.value+=chiffre;                                  /*on ajoute la valeur du bouton àla valeur de la case ecran */
-}
+class BaseCalculator {
+    constructor() {
+        this.ecran = document.getElementById("ecran");
+        this.res = document.getElementById("res");
+        this.list=[]
+    }
+  
+    ecrit(touche) {
+        if(this.ecran.value.length==0 && this.list.length>0){        //Si il y a eu un clear ou egal precedemment et que l'uttilisateur appui sur une touche la liste est réinitialisé
+            this.list=[]
+            this.list.push(touche)
+            this.ecran.value += touche;
+        }
+        else{                                                        //Sinon on ajoute juste à ecran et a liste la touche tapé
+            this.list.push(touche)
+            this.ecran.value += touche;
+        }
+    }
+  
+    calcul() {
+        let res = eval(this.ecran.value);
+        this.res.innerHTML = this.ecran.value + "=" + res;
+        this.ecran.value = "";
+    }
 
-function Boutonclear(){                                          /*la fonction clear retire le dernier élément de l'écran*/
-    var ecran = document.getElementById("ecran");                /*on créé un variable ecran afin de pouvoir modifier par la suite la valeur de la case*/
-    var valeurActuel =ecran.value;                               /*on créé une variable qui prend la valeur de l'écran*/
-    ecran.value= valeurActuel.slice(0,-1);                       /*on retire donc le dernier charactère de la chaîne*/
-}
-function calcul(){                                                /*la fonction calcul effectue le calcul lorsque la touche egal est préssé*/
-    var res = eval(ecran.value);                                  /*on créé la variable res avec comme valeur le résultat de notre clacul*/
-    document.getElementById("res").innerHTML=ecran.value+"="+res; /*On affiche le résultat dans la case prévu à cet effet*/
-    ecran.value="";                                               /*on réinitialise l'ecran*/
-}
-function ALLclear(){              /*la fonction ALLclear efface tout les charactère*/
-    ecran.value="";               /*on réinitialise l'ecran*/
-}
+    retour(){
+        if(this.ecran.value.length==this.list.length){           //Dans le cas ou il n'y a pas eu de clear ou égal on efface juste le dernier caractère
+            this.list.pop();
+            let valeurActuel =this.ecran.value;                               
+            this.ecran.value= valeurActuel.slice(0,-1); 
+        }
+        else{                                                    //Si il y a eu un clear ou égal alors on reprends les valeur stocké dans la liste et on les affiche de nouveau
+            for(let i=0;i<this.list.length;i++){
+                this.ecran.value+=this.list[i]
+            }
+        }
+    }
+
+    ALLclear() {
+        this.ecran.value = "";
+    }
+  }
+  let baseCalculator = new BaseCalculator();
 
